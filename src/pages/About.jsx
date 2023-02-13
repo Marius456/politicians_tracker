42aff9c172cff356ac5d9a14a17a4c0e704e0280
Marius <marius.zilguzis@ktu.edu>
2023-02-13 10:44:11 +0200

    decorated about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,22 +1,26 @@
 import "./About.css";
 
-export function About(){
-    return(
+export function About() {
+    return (
         <>
-            <h1>Apie</h1>
             <div className="box">
-                <div className="title">
-                    <span>Titulinis</span>
+                <div className="details">
+                    <div className="title">
+                        <h1>Apie Projektą</h1>
+                    </div>
+                    <div className="text">
+                        <p>Projektas sukurtas Mariaus Žilgužio</p>
+                        <a href="https://www.linkedin.com/in/marius-%C5%BEilgu%C5%BEis-827173242/">LinkedIn nuoroda</a>
+                        <p>Sukūriau šį projektą naudodamas ReactJS <i>framework</i>.</p>
+                        <a href="https://github.com/Marius456/politicians_tracker">GitHub nuoroda</a>
+                        <p>Duomenys surinkti naudojant savo sukurta programa kurioje naudojau Python programavimo kalbą. Duomenys paimti iš oficialios seimo ir mokesčių inspekcijos svetainės ir išsaugoti JSON formatu.</p>
+                        <a href="https://github.com/Marius456/scrapper">Github nuoroda</a>
+                    </div>
                 </div>
-                <div className="text">
-                    <p>Projektas sukurtas Mariaus Žilgužio</p>
-                    <a href="https://www.linkedin.com/in/marius-%C5%BEilgu%C5%BEis-827173242/">LinkedIn nuoroda</a>
-                    <p>Sukūriau šį projektą naudodamas ReactJS <i>framework</i>.</p>
-                    <a href="https://github.com/Marius456/politicians_tracker">GitHub nuoroda</a>
-                    <p>Duomenys surinkti naudojant savo sukurta programa kurioje naudojau Python programavimo kalbą. Duomenys paimti iš oficialios seimo ir mokesčių inspekcijos svetainės ir išsaugoti JSON formatu.</p>
-                    <a href="https://github.com/Marius456/scrapper">Github nuoroda</a>
+                <div className="imagediv">
+                    <img src="/imgs/nykis.jpg" className="image"/>
                 </div>
             </div>
-        </> 
+        </>
     )
 }
